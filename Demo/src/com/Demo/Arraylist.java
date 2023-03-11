a87a8990ee99d3e52c9b919afd45e2cdb8362d1b
Akash <akashzambre7219@gmail.com>
2023-03-11 16:16:41 +0530

    This is arraylist demo code

---
+++ Demo/src/com/Demo/Arraylist.java
@@ -0,0 +1,15 @@
+package com.Demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		List<Integer> list=Arrays.asList(2,3,4,5,7,9);
+		
+		System.out.println(list);
+
+	}
+
+}
